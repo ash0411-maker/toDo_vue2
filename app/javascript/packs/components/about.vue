13fe6014d14cfba869c00c7d256f49f91840a664
ash <satoshi.0411@icloud.com>
2021-04-04 07:19:26 +0900

    [add] components

---
+++ app/javascript/packs/components/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- 内容はお好みで -->
+    <p>This is a sample of TODO application with Vue.js and Ruby on Rails.</p>
+    <p>Sample code is <a href="https://www.netflix.com/browse" target="_blank">Click Here</a></p>
+  </div>
+</template>
