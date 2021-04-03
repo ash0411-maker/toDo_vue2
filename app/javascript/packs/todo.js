ab4e6caf5111cd0df38b689bcaf399919896a1c7
ash <satoshi.0411@icloud.com>
2021-04-04 07:48:12 +0900

    [add] router-view

--- app/javascript/packs/todo.js
+++ app/javascript/packs/todo.js
@@ -1,8 +1,11 @@
 import Vue from "vue/dist/vue.esm.js"
+import Router from './router/router'
 import Header from "./components/header"
+
 
 let app = new Vue({
   el: "#app",
+  router: Router,
   components: {
     'navbar': Header,
   }
